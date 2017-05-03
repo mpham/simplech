0c395e7ffcef7cb5599dfff5f182998da7a28a35
Michael Pham <jikerz@gmail.com>
2017-05-03 16:13:14 -0500

    Initial Chats controller/endpoints

---
+++ SimpleChat/Controllers/ChatsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SimpleChat.Controllers
+{
+    public class ChatsController : ApiController
+    {
+        [HttpGet]
+        [Route("chats")]
+        public async Task<IHttpActionResult> ListChats()
+        {
+            return Ok("done");
+        }
+
+        [HttpPost]
+        [Route("chats")]
+        public async Task<IHttpActionResult> CreateChat()
+        {
+            return Ok("done");
+        }
+
+        [HttpPatch]
+        [Route("chats/{id}")]
+        public async Task<IHttpActionResult> UpdateChat()
+        {
+            return Ok("done");
+        }
+    }
+}
